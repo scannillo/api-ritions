501e9d9420d5b3006c39c78ae61a13d0842ef39a
Sammy Cannillo <sammyjaynecannillo@gmail.com>
2024-10-18 21:02:51 -0500

    Clean up endpoint names; get red color working

--- app.py
+++ app.py
@@ -3,41 +3,42 @@ from kasa import Discover, Module
 
 app = Flask(__name__)
 
-@app.route("/light_off")
-async def hello_world():
-    someThing()
-    await lights()
-    print("Hello, world!")
-    return "<p>Hello, World!</p>"
+@app.route("/white")
+async def turnLightWhite():
+    dev = await Discover.discover_single("192.168.0.137",username="sammyjaynecannillo@gmail.com",password="X35zjpenn123!")
+    await dev.turn_on()
+    await dev.update()
+    light = dev.modules[Module.Light]
+    await light.set_hsv(0, 0, 100)
+#    await light.set_color_temp(4500) consider warmer white
+    await dev.update()
+    return "<p>WHITE!</p>"
 
+@app.route("/red")
+async def turnLightRed():
+    dev = await Discover.discover_single("192.168.0.137",username="sammyjaynecannillo@gmail.com",password="X35zjpenn123!")
+    await dev.turn_on()
+    await dev.update()
+    light = dev.modules[Module.Light]
+    await light.set_hsv(0, 100, 100)
+    await dev.update()
+    return "<p>RED!</p>"
+
+@app.route("/light_off")
+async def turnLightOff():
+    dev = await Discover.discover_single("192.168.0.137",username="sammyjaynecannillo@gmail.com",password="X35zjpenn123!")
+    await dev.turn_off()
+    await dev.update()
+    return "<p>LIGHT OFF!</p>"
 
 @app.route("/light_on")
-async def light_on2():
-    someThing()
+async def turnLightOn():
     dev = await Discover.discover_single("192.168.0.137",username="sammyjaynecannillo@gmail.com",password="X35zjpenn123!")
     await dev.turn_on()
     await dev.update()
     print(dev.alias)
     print("Hello, world!")
-    return "<p>Hello, World!</p>"
-    
-@app.route("/light_red")
-async def hello_red():
-    dev = await Discover.discover_single("192.168.0.137",username="sammyjaynecannillo@gmail.com",password="X35zjpenn123!")
-    print(dev.modules)
-#    await light.set_color_temp(3000)
-    await dev.update()
-    print(dev.alias)
-    return "<p>Hello, RED WORLD!</p>"
-    
-def someThing():
-    print("More things")
-    
-async def lights():
-    dev = await Discover.discover_single("192.168.0.137",username="sammyjaynecannillo@gmail.com",password="X35zjpenn123!")
-    await dev.turn_off()
-    await dev.update()
-    print(dev.alias)
+    return "<p>LIGHT ON!</p>"
 
 #https://python-kasa.readthedocs.io/en/latest/guides/light.html FOR TOMORROW
 
