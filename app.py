da2e5ba2ea64222208ea21821617aa6f7a44e6a2
Sammy Cannillo <sammyjaynecannillo@gmail.com>
2024-10-20 21:50:04 -0500

    Add additional levels of horror tease

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 import random
 import time
 from pygame import mixer
+import playsound
 
 app = Flask(__name__)
 
@@ -47,14 +48,22 @@ async def turnLightRed():
         await dev.update()
     return "<p>RED!</p>"
     
+async def turnLightOff():
+    devices = await configureKasaLights()
+    print(devices)
+    for dev in devices.values():
+        await dev.turn_off()
+        await dev.update()
+    return "<p>LIGHTS OFF!</p>"
+    
 async def configureKasaLights() -> [Device]:
-    return await Discover.discover(username="sammyjaynecannillo@gmail.com",password="X35zjpenn123!")
-#    ip1 = "192.168.0.137"
-#    light1 = await Discover.discover_single(ip1,username="sammyjaynecannillo@gmail.com",password="X35zjpenn123!")
-#    
-#    ip2 = "192.168.0.28"
-#    light2 = await Discover.discover_single(ip2,username="sammyjaynecannillo@gmail.com",password="X35zjpenn123!")
-#    return {ip1:light1, ip2:light2}
+#    return await Discover.discover(username="sammyjaynecannillo@gmail.com",password="X35zjpenn123!")
+    ip1 = "192.168.0.137"
+    light1 = await Discover.discover_single(ip1,username="sammyjaynecannillo@gmail.com",password="X35zjpenn123!")
+    
+    ip2 = "192.168.0.28"
+    light2 = await Discover.discover_single(ip2,username="sammyjaynecannillo@gmail.com",password="X35zjpenn123!")
+    return {ip1:light1, ip2:light2}
 
 async def turnPlugOn():
     response = requests.get("https://www.virtualsmarthome.xyz/url_routine_trigger/activate.php?trigger=c3d1918e-a61b-4a40-a85b-d660418bdd51&token=e09d2de6-c030-48a4-8574-2e9c39e9b6f1&response=json")
@@ -77,12 +86,37 @@ async def triggerHorrorTease():
     await turnPlugOn()
     mixer.music.play()
     if length:
-        print("123")
         time.sleep(int(length))
     else:
-        print("345")
-        time.sleep(random.randint(2, 5))
+        time.sleep(7)
     await turnPlugOff()
+    await turnLightWhite()
+    return "<p>HORROR TEASED!</p>"
+    
+@app.route("/murder_tease")
+async def triggerMurderTease():
+    length = request.args.get("length")
+
+    mixer.init()
+    mixer.music.load("chime_incorrect.wav")
+    mixer.music.play()
+#    playsound.playsound("chime_incorrect.wav")
+    time.sleep(3)
+    await turnLightOff()
+    await turnPlugOn()
+    time.sleep(5)
+    await turnPlugOff()
+    await turnLightWhite()
+    return "<p>MURDER TEASED!</p>"
+    
+@app.route("/baby_tease")
+async def triggerBabyTease():
+    mixer.init()
+    mixer.music.load("long_sweep.wav")
+
+    await turnLightRed() # if this fails, strobe doesn't turn off.
+    mixer.music.play()
+    time.sleep(7)
     await turnLightWhite()
     return "<p>HORROR TEASED!</p>"
     
